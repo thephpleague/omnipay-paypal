8153ce8a05bd2b177ed1f8a5c279ffd3b75f9d64
Jason Varga <jason@pixelfear.com>
2014-07-27 15:24:56 -0400

    Add line items to request

--- src/Message/ExpressCompleteAuthorizeRequest.php
+++ src/Message/ExpressCompleteAuthorizeRequest.php
@@ -20,6 +20,16 @@ class ExpressCompleteAuthorizeRequest extends AbstractRequest
         $data['PAYMENTREQUEST_0_DESC'] = $this->getDescription();
         $data['PAYMENTREQUEST_0_NOTIFYURL'] = $this->getNotifyUrl();
 
+        $items = $this->getItems();
+        if ($items) {
+            foreach ($items as $n => $item) {
+                $data["L_PAYMENTREQUEST_0_NAME$n"] = $item->getName();
+                $data["L_PAYMENTREQUEST_0_DESC$n"] = $item->getDescription();
+                $data["L_PAYMENTREQUEST_0_QTY$n"] = $item->getQuantity();
+                $data["L_PAYMENTREQUEST_0_AMT$n"] = $this->formatCurrency($item->getPrice());
+            }
+        }
+
         $data['TOKEN'] = $this->httpRequest->query->get('token');
         $data['PAYERID'] = $this->httpRequest->query->get('PayerID');
 
