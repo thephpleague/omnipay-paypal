0c93059fe8122e16399ad76bbc318d79e82fd8fd
Tai Poh Nean <pohnean@gmail.com>
2015-02-14 11:15:06 +0800

    Adding toJSON() function in AbstractRestRequest.

--- src/Message/AbstractRestRequest.php
+++ src/Message/AbstractRestRequest.php
@@ -136,7 +136,7 @@ abstract class AbstractRestRequest extends \Omnipay\Common\Message\AbstractReque
                     'Authorization' => 'Bearer ' . $this->getToken(),
                     'Content-type'  => 'application/json',
                 ),
-                json_encode($data)
+                $this->toJSON($data)
             );
         }
         
@@ -148,5 +148,25 @@ abstract class AbstractRestRequest extends \Omnipay\Common\Message\AbstractReque
         $httpResponse = $httpRequest->send();
 
         return $this->response = new RestResponse($this, $httpResponse->json(), $httpResponse->getStatusCode());
+    }
+
+    /**
+     * Returns object JSON representation required by PayPal.
+     * The PayPal REST API requires the use of JSON_UNESCAPED_SLASHES.
+     *
+     * Adapted from the official PayPal REST API PHP SDK (https://github.com/paypal/PayPal-PHP-SDK/blob/master/lib/PayPal/Common/PayPalModel.php)
+     *
+     * @param int $options http://php.net/manual/en/json.constants.php
+     * @return string
+     */
+    public function toJSON($data, $options = 0)
+    {
+        // Because of PHP Version 5.3, we cannot use JSON_UNESCAPED_SLASHES option
+        // Instead we would use the str_replace command for now.
+        // TODO: Replace this code with return json_encode($this->toArray(), $options | 64); once we support PHP >= 5.4
+        if (version_compare(phpversion(), '5.4.0', '>=') === true) {
+            return json_encode($data, $options | 64);
+        }
+        return str_replace('\\/', '/', json_encode($data, $options));
     }
 }
