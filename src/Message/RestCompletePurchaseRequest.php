f710041732525fac1265e0a81fd11d5b93835f63
Tai Poh Nean <pohnean@gmail.com>
2015-02-15 08:12:33 +0800

    Adding RestCompletePurchaseRequest class

---
+++ src/Message/RestCompletePurchaseRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Omnipay\PayPal\Message;
+
+
+class RestCompletePurchaseRequest extends AbstractRestRequest {
+    /**
+     * Get the raw data array for this message. The format of this varies from gateway to
+     * gateway, but will usually be either an associative array, or a SimpleXMLElement.
+     *
+     * @return mixed
+     */
+    public function getData()
+    {
+        $this->validate('transactionReference');
+
+        return [];
+    }
+
+    public function getEndpoint()
+    {
+        return parent::getEndpoint() . '/payments/payment/' . $this->getTransactionReference() . '/execute';
+    }
+}
