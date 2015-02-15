5db0be5bd9c24918f01539a3c20c89e97c57c87e
Tai Poh Nean <pohnean@gmail.com>
2015-02-15 17:36:58 +0800

    Adding tests for RestCompletePurchase responses

---
+++ tests/Message/RestAuthorizeResponseTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Omnipay\PayPal\Message;
+
+
+use Omnipay\Tests\TestCase;
+
+class RestAuthorizeResponseTest extends TestCase {
+    public function testConstruct() {
+
+    }
+
+    public function testPurchaseRedirectSuccess() {
+
+    }
+
+    public function testPurchaseRedirectFailure() {
+
+    }
+}
