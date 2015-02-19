92ddaac4470ab95a14b566bbeb56f3230475729e
Rajesh Sharma <broncha@rajesharma.com>
2015-02-19 23:55:34 +0545

    send all other amounts along for complete purchase

--- src/Message/ExpressCompleteAuthorizeRequest.php
+++ src/Message/ExpressCompleteAuthorizeRequest.php
@@ -20,6 +20,13 @@ class ExpressCompleteAuthorizeRequest extends AbstractRequest
         $data['PAYMENTREQUEST_0_DESC'] = $this->getDescription();
         $data['PAYMENTREQUEST_0_NOTIFYURL'] = $this->getNotifyUrl();
 
+        $data['MAXAMT'] = $this->getMaxAmount();
+        $data['PAYMENTREQUEST_0_TAXAMT'] = $this->getTaxAmount();
+        $data['PAYMENTREQUEST_0_SHIPPINGAMT'] = $this->getShippingAmount();
+        $data['PAYMENTREQUEST_0_HANDLINGAMT'] = $this->getHandlingAmount();
+        $data['PAYMENTREQUEST_0_SHIPDISCAMT'] = $this->getShippingDiscount();
+        $data['PAYMENTREQUEST_0_INSURANCEAMT'] = $this->getInsuranceAmount();
+
         $data['TOKEN'] = $this->getToken() ? $this->getToken() : $this->httpRequest->query->get('token');
         $data['PAYERID'] = $this->getPayerID() ? $this->getPayerID() : $this->httpRequest->query->get('PayerID');
 
