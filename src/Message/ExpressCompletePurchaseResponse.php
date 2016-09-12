9cf6cd16d7ed743678b66b8a49522bb00b234b80
Luke Holder <lukemh@gmail.com>
2016-09-12 21:01:17 +0800

    Need to also allow for normal success.

--- src/Message/ExpressCompletePurchaseResponse.php
+++ src/Message/ExpressCompletePurchaseResponse.php
@@ -7,6 +7,12 @@ namespace Omnipay\PayPal\Message;
  */
 class ExpressCompletePurchaseResponse extends ExpressAuthorizeResponse
 {
+	public function isSuccessful()
+	{
+		$success = isset($this->data['ACK']) && in_array($this->data['ACK'], array('Success', 'SuccessWithWarning'));
+		return !$this->isRedirect() && $success;
+	}
+
     public function isRedirect()
     {
         return isset($this->data['L_ERRORCODE0']) && in_array($this->data['L_ERRORCODE0'], array('10486'));
