016403c8bb6fc369356efffb99709f10f247b7c4
Marco Hillger <marco@seofood.de>
2018-01-05 13:39:46 +0100

    added invoice_number to REST API

--- tests/RestGatewayTest.php
+++ tests/RestGatewayTest.php
@@ -31,7 +31,7 @@ class RestGatewayTest extends GatewayTestCase
                 'lastName' => 'User',
                 'number' => '4111111111111111',
                 'expiryMonth' => '12',
-                'expiryYear' => '2017',
+                'expiryYear' => date('Y'),
                 'cvv' => '123',
             )),
         );
