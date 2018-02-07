b9a681ff483adc24f074118e94dea10fd38dd624
jlttt <jlttt@laposte.net>
2018-02-07 21:36:50 +0100

    add comments on supported shipping preference values

--- src/Message/RestPurchaseRequest.php
+++ src/Message/RestPurchaseRequest.php
@@ -236,6 +236,22 @@ class RestPurchaseRequest extends RestAuthorizeRequest
         return $this->getParameter('shippingPreference');
     }
 
+    /**
+     * Set the shipping preference
+     *
+     * Supported values:
+     * 'NO_SHIPPING': redacts shipping address fields from the PayPal pages.
+     * Recommended value to use for digital goods.
+     * 'GET_FROM_FILE': Get the shipping address selected by the buyer on PayPal pages.
+     * 'SET_PROVIDED_ADDRESS' (not yet fully supported by the library since shipping address can't be provided):
+     * Use the address provided by the merchant. Buyer is not able to change the address on the PayPal pages.
+     * If merchant doesn't pass an address, buyer has the option to choose the address on PayPal pages.
+     *
+     * @param  string $value
+     * @return bool.
+     * @link https://developer.paypal.com/docs/api/orders/#definition-application_context
+     * @link https://www.paypalobjects.com/api/checkout.js
+     */
     public function setShippingPreference($value)
     {
         return $this->setParameter('shippingPreference', $value);
