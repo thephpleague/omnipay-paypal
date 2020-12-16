05c7a38fee422bb60728b4cbddcfd2f158b993b4
DDEV-Local User <nobody@example.com>
2020-12-16 06:44:04 +0000

    Add getProcessorFeeAmount and update tests/sample data.

--- src/Message/RestResponse.php
+++ src/Message/RestResponse.php
@@ -52,6 +52,33 @@ class RestResponse extends AbstractResponse
         return null;
     }
 
+    /*
+     * The fee taken by PayPal for the transaction. Available from the Sale
+     * object in RelatedResources only once a transaction has been completed
+     * and funds have been received by merchant.
+     *
+     *  https://developer.paypal.com/docs/api/payments/v1/#definition-sale
+     *
+     *
+     * There may be a 'fee' field associated with amount details object but
+     * this seems to pertain to a handling_fee charged to client rather than
+     * the PayPal processing fee.
+     *
+     *  https://developer.paypal.com/docs/api/payments/v1/#definition-details
+     */
+    public function getProcessorFeeAmount()
+    {
+        if (!empty($this->data['transactions']) &&
+            !empty($this->data['transactions'][0]['related_resources']) &&
+            !empty($this->data['transactions'][0]['related_resources'][0]['sale']) &&
+            !empty($this->data['transactions'][0]['related_resources'][0]['sale']['transaction_fee']))
+        {
+            return $this->data['transactions'][0]['related_resources'][0]['sale']['transaction_fee']['value'];
+        }
+
+        return null;
+    }
+
     public function getMessage()
     {
         if (isset($this->data['error_description'])) {
